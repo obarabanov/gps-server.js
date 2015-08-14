b6e184cd7972e5b5b32389152096005343456770
Alex <oleksandr.barabanov@gmail.com>
2015-08-14 16:24:32 +0300

    DB-logic switched on pooled connections usage.

--- modules/db.js
+++ modules/db.js
@@ -13,7 +13,9 @@ if ((config.get('environment') == 'dev') || (config.get('env') == 'dev')) {
 	dbconf = config.get('db');
 }
 
-//	TODO:	use connection pool instead..
+//	using connection pool for better DB performance
+//	@see https://github.com/felixge/node-mysql#pooling-connections
+var pool  = mysql.createPool( dbconf );
 
 /**
  * It's just for testing DB connection.
@@ -22,8 +24,15 @@ if ((config.get('environment') == 'dev') || (config.get('env') == 'dev')) {
 db.ping_db = function ()
 {
 	var status = 'unclear';
-
+	
+	/*
+	//	straight connections-based approach
+	
 	var connection = mysql.createConnection(dbconf);
+	//	in case there are no per-query callbacks
+	connection.on('error', function (err) {
+		log.error(err.code);
+	});
 	connection.connect(function (err) {
 		if (err) {
 			log.error('error connecting: ' + err.stack);
@@ -41,10 +50,28 @@ db.ping_db = function ()
 		}
 		log.debug('connection: ' + connection.threadId + ' ended.');
 	});
+	*/
 
-
-	//  TODO:   wait for asynch queries procession
-	log.debug('status: ' + status);
+	//	pooled connections approach
+	pool.getConnection(function(err, connection) {
+		
+		if (err) {
+			log.error('error getting connection from pool: ' + err.stack);
+			status = 'connection error';
+			return;
+		}
+		
+		log.debug('connected as id: ' + connection.threadId);
+		
+		status = 'connected';
+		//log.debug('status: ' + status);
+		//  TODO:   announce status asynchronously
+		
+		// And done with the connection.
+		connection.release();
+		// Don't use the connection here, it has been returned to the pool.
+		
+	});
 	return status;
 };
 
@@ -52,13 +79,8 @@ db.save_into_db = function (data)
 {
 	var status = '';
 
+	/*
 	var connection = mysql.createConnection(dbconf);
-
-	//	in case there are no per-operation callbacks
-	connection.on('error', function (err) {
-		log.error(err.code);
-	});
-
 	connection.connect(function (err) {
 		if (err) {
 			log.error('error connecting: ' + err.stack);
@@ -68,37 +90,57 @@ db.save_into_db = function (data)
 		log.debug('connected as id: ' + connection.threadId);
 		status = 'connected';
 	});
+	*/
 
-	connection.query('SELECT * FROM gps Where number = ?', [data.number], function (err, rows, fields) {
-		if (err) throw err;
-
-		status += ', Rows found: ' + rows.length;
-		//log.debug(status + '\n', rows);
-		//log.debug('\nFields: \n', fields);
-
-		if (rows.length > 0)
-		{
-			data[ 'gps_id'] = rows[0].id;
-			log.info('GPS ID: ', rows[0].id);
-
-			connection.query('INSERT INTO geometries (_TIMESTAMP, type, geometry, creator_id, created, deleted, propertyA, propertyB, propertyC) VALUES (now(), "Point", GeomFromText( \'POINT(? ?)\'), 2, now(), 0, 0, 0, 0)', [parseFloat(data.longitude), parseFloat(data.latitude)], function (err, res) {
-				if (err)
-					throw err;
-				data[ 'geometry_id'] = res.insertId;
-				data[ 'creator_id'] = 2;
-				data[ 'created'] = new Date();
-				data[ 'deleted'] = 0;
-				log.info('Geometry ID: ', res.insertId);
-
-				connection.query('INSERT INTO gps_data SET ?', data, function (err, res) {
-					if (err)
-						throw err;
-					log.info('GPS Data ID: ', res.insertId);
-				});
-			});
+	pool.getConnection(function(err, connection) {
+		if (err) {
+			log.error('error getting connection from pool: ' + err.stack);
+			status = 'connection error';
+			return;
 		}
+		log.debug('connection id: ' + connection.threadId);
+		status = 'connected';
+		
+		connection.query('SELECT * FROM gps Where number = ?', 
+			[data.number], 
+			function (err, rows, fields) {
+				if (err) throw err;
+
+				status += ', Rows found: ' + rows.length;
+				//log.debug(status + '\n', rows);
+				//log.debug('\nFields: \n', fields);
+
+				if (rows.length > 0)
+				{
+					data[ 'gps_id'] = rows[0].id;
+					log.info('GPS ID: ', rows[0].id);
+
+					connection.query('INSERT INTO geometries (_TIMESTAMP, type, geometry, creator_id, created, deleted, propertyA, propertyB, propertyC) VALUES (now(), "Point", GeomFromText( \'POINT(? ?)\'), 2, now(), 0, 0, 0, 0)', [parseFloat(data.longitude), parseFloat(data.latitude)], function (err, res) {
+						if (err)
+							throw err;
+						data[ 'geometry_id'] = res.insertId;
+						data[ 'creator_id'] = 2;
+						data[ 'created'] = new Date();
+						data[ 'deleted'] = 0;
+						log.info('Geometry ID: ', res.insertId);
+
+						connection.query('INSERT INTO gps_data SET ?', data, function (err, res) {
+							if (err)
+								throw err;
+							log.info('GPS Data ID: ', res.insertId);
+						});
+					});
+				}
+			}
+		);
+		log.debug('status: ' + status);
+			
+		// And done with the connection.
+		connection.release();
+		// Don't use the connection here, it has been returned to the pool.
 	});
 
+	/*
 	//connection.end();
 	connection.end(function (err) {
 		if (err) {
@@ -106,15 +148,13 @@ db.save_into_db = function (data)
 		}
 		log.debug('connection: ' + connection.threadId + ' ended.');
 	});
-
-	log.info('status: ' + status);
-	return status;
+	*/
 };
 
 db.checkIMEI = function (socket, number)
 {
 	var status = 0;
-
+	/*
 	var connection = mysql.createConnection(dbconf);
 	connection.connect(function (err) {
 		if (err) {
@@ -123,23 +163,45 @@ db.checkIMEI = function (socket, number)
 		}
 		log.debug('connected as id: ' + connection.threadId);
 	});
+	*/
 
-	connection.query('SELECT * FROM gps Where number = ?', [number], function (err, rows, fields) {
-		if (err)
-			throw err;
-
-		if (rows.length > 0) {
-			status = 1;
-			socket.imei = number; // keep it in socket connection
-			socket.write(String.fromCharCode(0x01));
-			//socket.write('Welcome to the Telnet server!\n');
-			//socket.end();
-			log.info('GPS ID: ', rows[0].id, String.fromCharCode(0x01));
-		} else {
-			log.info('GPS IMEI: ', number, ' not found');
+	pool.getConnection(function(err, connection) {
+		if (err) {
+			log.error('error getting connection from pool: ' + err.stack);
+			return status;	//	FIXME:	is will return from: function(err, connection) - anonymous callback function. Not from checkIMEI() method.
+			//	TODO:	maybe close socket here ?
 		}
-	});
+		log.debug('connection id: ' + connection.threadId);
 
+		connection.query('SELECT * FROM gps Where number = ?', 
+			[number], 
+			function (err, rows, fields) {
+				if (err) {
+					//	TODO:	maybe close socket here ?
+					throw err;
+				}
+
+				if (rows.length > 0) {
+					status = 1;
+					socket.imei = number; // keep it in socket connection
+					socket.write(String.fromCharCode(0x01));
+					//socket.write('Welcome to the Telnet server!\n');
+					//socket.end();
+					log.info('GPS ID: ', rows[0].id, String.fromCharCode(0x01));
+				} else {
+					//	TODO:	close socket here, to infor device it's not registered and release socket resources.
+					log.info('GPS IMEI: ', number, ' not found');
+				}
+			}
+		);
+		log.debug('checkIMEI() status: ' + status);
+		
+		// And done with the connection.
+		connection.release();
+		// Don't use the connection here, it has been returned to the pool.
+	});
+	
+	/*
 	connection.end(function (err) {
 		// The connection is terminated now
 		if (err) {
@@ -147,5 +209,5 @@ db.checkIMEI = function (socket, number)
 		}
 		log.debug('connection: ' + connection.threadId + ' ended.');
 	});
-
+	*/
 };
